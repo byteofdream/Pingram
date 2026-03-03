00bf873452731da081c39deec5f4d7c744c3204d
DreamyByte Labs <dozirod@mail.ru>
2026-03-03 21:19:59 +0200

    Add main function to initialize QApplication

---
+++ app/main.cpp
@@ -0,0 +1,12 @@
+#include "ui/main_window.hpp"
+
+#include <QApplication>
+
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
+
+    pingram::ui::MainWindow window;
+    window.show();
+
+    return app.exec();
+}
